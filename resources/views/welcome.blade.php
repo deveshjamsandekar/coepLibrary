222767fee4b868c2a4f7685326368561e5049496
deveshjamsandekar <deveshj23@gmail.com>
2018-04-17 00:13:58 +0530

    Deploy code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to COEP Library System
+                    Welcome to COEP Library System Again
                 </div>
 
                 <div class="links">
