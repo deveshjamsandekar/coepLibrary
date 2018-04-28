88a5dcf218433d7262f0ff43bd710abb37ed6839
deveshjamsandekar <deveshj23@gmail.com>
2018-04-28 14:12:25 +0530

    new

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -12,8 +12,8 @@ public function send()
 {
   $data = array('name'=>"DJ", 'body' => "you have issued a book");
   Mail::send('email',$data, function($message){
-    $message->to('deveshj23@gmail.com','Devesh')->subject('Book Issued');
-    $message->from('librarycoep@gmail.com','COEP Library');
+  $message->to('deveshj23@gmail.com','Devesh')->subject('Book Issued');
+  $message->from('librarycoep@gmail.com','COEP Library');
   });  //
 
   // Mail::to('kvjkumr@gmail.com')->send(new WelComeMail);
